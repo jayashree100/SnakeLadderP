441c85e367b080d7846f22946ce1ae2782893f1d
JAISHRI <sjayashree529@gmail.com>
2022-03-07 12:53:08 +0530

    UC-2Snakeladder

--- src/com/bridgeLab/Snakeandladder.java
+++ src/com/bridgeLab/Snakeandladder.java
@@ -5,9 +5,10 @@ import java.io.*;
 public class Snakeandladder {
 	public static void main (String [] args)  throws IOException {
 		
-		 	int start = 0;
-		 	System.out.println("Starting position of player is: " + start);
-
+		int start = 0;
+		System.out.println("Starting position of player is: " + start);
+		int roll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+		System.out.println(roll);
     
         
     }
